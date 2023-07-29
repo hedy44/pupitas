c8a07016d86d3edbd742cb01316f1a7a4a785319
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-29 01:54:26 +0100

    chore: navbar component call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar'; // Importa o componente Navbar
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar /> {/* Exibe o componente Navbar na página */}
+      <h1>Bem-vindo à Pupitas</h1>
     </div>
   );
 }
