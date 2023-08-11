bd7463b187f835bbb28ac4677144c90b50e437f2
Hernani-Diniz <hdiniz4@gmail.com>
2023-08-11 20:34:29 +0100

    chore: navbar items sketch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,12 @@ import '../styles/Navbar.css';
 function Navbar(){
     return (
         <nav className="navbar">
-            <div className="logo">
-                <h1>Pupitas</h1>
+
+            <div className="nav-links">
+                <a href="#inicio">Início</a>
+                <a href="#roupas">Roupas</a>
+                <a href="#acessorios">Acessórios</a>
+                <a href="#carrinho">Carrinho</a>
             </div>
              {/* Adicionar os itens da barra de navegação aqui (ex.: links para outras páginas) */}
         </nav>
