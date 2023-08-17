befcdded488644c4120f6a9f2c7dba1e1d5cbc30
Hernani-Diniz <hdiniz4@gmail.com>
2023-08-17 01:37:47 +0100

    chore: file to data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const clothesCollection = [
+    {
+      id: 1,
+      name: 'Camiseta Unicórnio',
+      category: 'Menina',
+      price: 25.99,
+      image: 'https://example.com/camiseta-unicornio.jpg',
+    },
+    {
+      id: 2,
+      name: 'Calça Jeans Rasgada',
+      category: 'Menino',
+      price: 29.99,
+      image: 'https://example.com/calca-jeans-rasgada.jpg',
+    },
+    // Adicionar mais roupas aqui, se necessário
+  ];
+  
+  export const accessoriesCollection = [
+    {
+      id: 1,
+      name: 'Colar de Flores',
+      category: 'Acessório',
+      price: 9.99,
+      image: 'https://example.com/colar-flores.jpg',
+    },
+    {
+      id: 2,
+      name: 'Boné Estiloso',
+      category: 'Acessório',
+      price: 12.99,
+      image: 'https://example.com/bone-estiloso.jpg',
+    },
+    // Adicionar mais acessórios aqui, se necessário
+  ];
+  
