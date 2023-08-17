c7de736f17988f3c43e079a91e3292d7a088863c
Hernani-Diniz <hdiniz4@gmail.com>
2023-08-17 01:38:32 +0100

    chore: home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Bem-vindo à Pupitas</h2>
+      <p>Aqui você encontra roupas casuais para meninos e meninas.</p>
+    </div>
+  );
+}
+
+export default HomePage;
