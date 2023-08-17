6a73fea8d051e2dd19e38f95610c425f3fabc942
Hernani-Diniz <hdiniz4@gmail.com>
2023-08-17 01:38:11 +0100

    chore: page of the cart

---
+++ src/components/CartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CartPage() {
+  return (
+    <div>
+      <h2>Carrinho de Compras</h2>
+      {/* Adicione aqui a lógica para mostrar os itens no carrinho */}
+    </div>
+  );
+}
+
+export default CartPage;
