2522270163250bb3d4303bf0337313d63851979c
Hernani Diniz <hdiniz4@gmail.com>
2024-11-17 21:03:37 +0000

    Component Products initial structure

---
+++ src/components/Produtos.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Produtos() {
+  return (
+    <div>
+      <h1>Produtos da loja</h1>
+      <div>
+        <h2>Produto 1</h2>
+        <p>Descrição do produto 1</p>
+        <button>Reservar</button>
+      </div>
+      <div>
+        <h2>Produto 2</h2>
+        <p>Descrição do produto 2</p>
+        <button>Reservar</button>
+      </div>
+      <div>
+        <h2>Produto 3</h2>
+        <p>Descrição do produto 3</p>
+        <button>Reservar</button>
+      </div>
+    </div>
+  );
+}
+
+export default Produtos;
