6a9c7c955c0fed94f9c7f00749beff7c14740e87
hedy44 <hdiniz4@gmail.com>
2025-02-17 18:11:32 +0000

    Add Navbar component to App.js for global usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+
 import Produtos from './components/Produtos';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       </div>
     
