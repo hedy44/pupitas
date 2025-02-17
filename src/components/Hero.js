56dcf05a8252d5129fc85d2c6df312de8d293bea
hedy44 <hdiniz4@gmail.com>
2025-02-17 18:10:04 +0000

    Create Hero component and move header structure with its own CSS

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Hero.css';
+
+function Hero() {
+  return (
+    <header className="header">
+      <h1>Bem-vindo à Maria Pupitas!</h1>
+      <p>Encontre os melhores produtos para seus filhos!</p>
+    </header>
+  );
+}
+
+export default Hero;
