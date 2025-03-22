93156bbda414f5e34c8cae33e47c1f86baa0bff9
hedy44 <hdiniz4@gmail.com>
2025-03-22 14:19:33 +0000

    cleaning js

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -6,12 +6,7 @@ function Contato() {
     <section className="contato">
       <h2>Entre em Contato</h2>
       <p>Envie-nos uma mensagem ou entre em contato pelas redes sociais.</p>
-      <form>
-        <input type="text" placeholder="Seu nome" />
-        <input type="email" placeholder="Seu e-mail" />
-        <textarea placeholder="Sua mensagem"></textarea>
-        <button type="submit">Enviar</button>
-      </form>
+     
     </section>
   );
 }
